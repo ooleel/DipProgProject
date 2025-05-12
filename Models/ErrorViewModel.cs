ae96739b17b8de554e6df58f1722b0189b4107b3
Leeloo <leeloo.schaff@gmail.com>
2025-05-12 18:06:40 +1000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeniorLearnWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
